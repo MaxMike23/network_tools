dd6dc6da8fd43010e0b8e7334822dce66d69082c
MaxMike23 <michaelhansen19@gmail.com>
2023-08-11 13:15:33 -0400

    Added tests_network_tools.py and first test

--- tests/test_network_tools.py
+++ tests/test_network_tools.py
@@ -1,4 +1,13 @@
+import unittest
+import subprocess
+from NetworkTools.network_tools import *
 
+class TestNetworkTools(unittest.TestCase):
+    """ Test NetworkTools """
+
+    def test_interface_is_connected(self):
+        """ Tests for active interfaces """
+        self.assertEqual(interface_is_connected(), True)
 
 
 
